38867143963c59659e515af81c27112df57972af
Chris Tang <christang_1023@outlook.com>
2021-03-13 00:46:24 +0800

    Add configuration and base codes for ui

---
+++ src/app/src/main/java/com/example/healthtracker/interfaces/LoginUI.java
@@ -0,0 +1,49 @@
+package com.example.healthtracker.interfaces;
+
+public class LoginUI extends UI {
+    public LoginUI() {
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void printErrorMessage() {
+
+    }
+
+    @Override
+    public void printSuccessfulMessage() {
+
+    }
+
+    /*
+    Triggers auto login by getting username and password (e.g. remember me) from local cache file.
+     */
+    public boolean autoLogin() {
+        return false;
+    }
+
+    /*
+    Triggers login by Facebook.
+     */
+    public void loginViaFacebook() {
+
+    }
+
+    /*
+    Triggers login by Google.
+     */
+    public void loginViaGoogle() {
+
+    }
+
+    /*
+    Triggers to submit the user's input login info to the server.
+     */
+    private void submitLoginInfo() {
+
+    }
+}
---
+++ src/app/src/main/java/com/example/healthtracker/interfaces/VerificationUI.java
@@ -0,0 +1,28 @@
+package com.example.healthtracker.interfaces;
+
+public class VerificationUI extends UI{
+    public VerificationUI() {
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void printErrorMessage() {
+
+    }
+
+    @Override
+    public void printSuccessfulMessage() {
+
+    }
+
+    /*
+    Submits the user input of verification code for app to verify it.
+     */
+    private void submitVerificationCode() {
+
+    }
+}
