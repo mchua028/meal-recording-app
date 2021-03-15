37ab97705c090589a8d9d6f2828f83d33b2d8735
Chris Tang <christang_1023@outlook.com>
2021-03-15 15:58:15 +0800

    Add skeleton for account subsystem

---
+++ src/app/src/main/java/com/example/healthtracker/data_access/SecurityVerifier.java
@@ -0,0 +1,17 @@
+package com.example.healthtracker.data_access;
+
+public class SecurityVerifier {
+    private String correctCode;
+
+    public SecurityVerifier() {
+    }
+
+    public void sendVerifyEmail() {
+
+    }
+
+    public boolean verifyCode(String inputCode) {
+
+    }
+
+}
---
+++ src/app/src/main/java/com/example/healthtracker/logic/PersonalInfoManager.java
@@ -0,0 +1,13 @@
+package com.example.healthtracker.logic;
+
+import java.util.HashMap;
+
+public class PersonalInfoManager {
+    public PersonalInfoManager() {
+    }
+
+    public void updateHealthInfo(HashMap<String, String> healthInfo) {
+
+    }
+
+}
