f28066481277001b2688ee50e22e902bef30091d
Chris Tang <christang_1023@outlook.com>
2021-03-22 01:52:06 +0800

    Add skeleton for Health info set-up/edit

---
+++ src/app/src/main/java/com/example/healthtracker/interfaces/InfoSetUpUI.java
@@ -0,0 +1,28 @@
+package com.example.healthtracker.interfaces;
+
+public class InfoSetUpUI extends UI {
+
+	@Override
+	public void display() {
+
+	}
+
+	@Override
+	public void printErrorMessage() {
+
+	}
+
+	@Override
+	public void printSuccessfulMessage() {
+
+	}
+
+	/**
+	 * throw exceptions
+	 */
+	private void submitHealthInfo() {
+		// TODO - implement InfoSetupUI.submitHealthInfo
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/app/src/main/java/com/example/healthtracker/logic/HealthInfoManager.java
@@ -0,0 +1,33 @@
+package com.example.healthtracker.logic;
+
+import java.util.HashMap;
+
+
+public class HealthInfoManager {
+
+	private static HealthInfoManager singleton = null;
+
+	public HealthInfo getHealthInfo() {
+		// TODO - implement com.example.healthtracker.logic.HealthInfoManager.getHealthInfo
+		throw new UnsupportedOperationException();
+	}
+
+	public static HealthInfoManager getSingleton() {
+		return singleton;
+	}
+
+	private HealthInfoManager HealthInfoManager() {
+		// TODO - implement com.example.healthtracker.logic.HealthInfoManager.com.example.healthtracker.logic.HealthInfoManager
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param info
+	 */
+	public void setHealthInfo(HashMap<String, String> info) {
+		// TODO - implement com.example.healthtracker.logic.HealthInfoManager.setHealthInfo
+		throw new UnsupportedOperationException();
+	}
+
+}
