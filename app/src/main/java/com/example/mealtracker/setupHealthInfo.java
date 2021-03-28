4d7b2624a9d51773f5104eac3627e3476ac57602
potassiumnitrate <berlynn_neo@hotmail.com>
2021-03-29 00:01:24 +0800

    Add Set Up Health Info UI after creating account (Register)

---
+++ app/src/main/java/com/example/mealtracker/setupHealthInfo.java
@@ -0,0 +1,89 @@
+package com.example.mealtracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class setupHealthInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_health_info);
+
+        // Gender dropdown box
+        Spinner gender_spinner = (Spinner) findViewById(R.id.gender_spinner);
+        List<String> gender_spinner_values = new ArrayList<>();
+        gender_spinner_values.add(0, "Select a gender ");
+        gender_spinner_values.add("Male");
+        gender_spinner_values.add("Female");
+        gender_spinner_values.add("Other");
+
+        ArrayAdapter<String> gender_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, gender_spinner_values);
+        gender_adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        gender_spinner.setAdapter(gender_adapter);
+        gender_spinner.setPrompt("Gender: ");
+        gender_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (parent.getItemAtPosition(position).equals("Select a gender ")) {
+                } else {
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected gender: " + item, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        // Activity dropdown box
+        Spinner activity_spinner = (Spinner) findViewById(R.id.activity_spinner);
+        List<String> activity_spinner_values = new ArrayList<>();
+        activity_spinner_values.add(0, "Select an activity ");
+        activity_spinner_values.add("None");
+        activity_spinner_values.add("Little");
+        activity_spinner_values.add("Moderate");
+        activity_spinner_values.add("High");
+
+        ArrayAdapter<String> activity_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, activity_spinner_values);
+        activity_adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        activity_spinner.setAdapter(activity_adapter);
+        activity_spinner.setPrompt("Activity: ");
+        activity_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (parent.getItemAtPosition(position).equals("Select an activity ")) {
+                } else {
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(), "Selected activity: " + item, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    public void onSubmitBtnClick (View view) {
+        // TODO: go to control logic - check input
+        Toast.makeText(view.getContext(), "Registration successful", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        // TODO: save to database or change database data
+    }
+
+}
