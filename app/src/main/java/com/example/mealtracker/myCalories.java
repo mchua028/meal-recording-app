cb868ce5ff77116b32fd8527c1b58462e714c47a
potassiumnitrate <berlynn_neo@hotmail.com>
2021-03-30 03:38:06 +0800

    Add Edit Calories UI Skeleton

--- app/src/main/java/com/example/mealtracker/myCalories.java
+++ app/src/main/java/com/example/mealtracker/myCalories.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -44,6 +45,25 @@ public class myCalories extends Fragment {
             }
 
         });
+
+        // Input Meal Details button
+        Button inputMealBtn = (Button) v.findViewById(R.id.inputMealBtn);
+        inputMealBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), manualInput.class));
+            }
+        });
+
+        // Edit Calories button
+        ImageButton imageButton = (ImageButton) v.findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), EditCalories.class));
+            }
+        });
+
         return v;
     }
 }
