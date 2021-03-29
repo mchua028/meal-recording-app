e74a5ee3a545cb4bce8187cc0ccc067fc524f6c2
potassiumnitrate <berlynn_neo@hotmail.com>
2021-03-30 00:52:43 +0800

    Add access to camera

---
+++ app/src/main/java/com/example/mealtracker/CameraFragment.java
@@ -0,0 +1,64 @@
+package com.example.mealtracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CameraFragment extends Fragment {
+
+    private static final String TAG = "CameraFragment";
+
+    // constant
+    private static final int INSTANT_FRAGMENT_NUM = 0;
+    private static final int IMPORT_FRAGMENT_NUM = 1;
+    private static final int CAMERA_REQUEST_CODE = 5;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_calories_camera, container, false);
+        Log.d(TAG, "onCreateView: started.");
+
+        Button launchCameraBtn = (Button) view.findViewById(R.id.launchCameraBtn);
+        launchCameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Log.d(TAG, "onClick: launching camera.");
+
+                //if (((uploadPicture)getActivity()).getCurrentTabNumber() == uploadPicture.INSTANT_FRAGMENT_ID) {
+                    //Log.d(TAG, "onClick: first if");
+                    if (((uploadPicture)getActivity()).checkPermissions(Permissions.CAMERA_PERMISSION[0])) {
+                        Log.d(TAG, "onClick: starting camera.");
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+                    } else {
+                        Intent intent = new Intent(getActivity(), uploadPicture.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+                //}
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Log.d(TAG, "onActivityResult: done taking a photo.");
+            Log.d(TAG, "onActivityResult: attempting to navigate to final share screen.");
+            // navigate to the final share screen to publish photo
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mealtracker/Permissions.java
@@ -0,0 +1,24 @@
+package com.example.mealtracker;
+
+import android.Manifest;
+
+public class Permissions {
+
+    public static final String[] PERMISSIONS = {
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.CAMERA
+    };
+
+    public static final String[] CAMERA_PERMISSION = {
+            android.Manifest.permission.CAMERA
+    };
+
+    public static final String[] WRITE_STORAGE_PERMISSION = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    public static final String[] READ_STORAGE_PERMISSION = {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+}
