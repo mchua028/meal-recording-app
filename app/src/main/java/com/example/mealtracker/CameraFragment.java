eefbce9e05fd8779fbdb3083c58c463696a8748d
potassiumnitrate <berlynn_neo@hotmail.com>
2021-04-06 01:07:55 +0800

    Display image from camera

--- app/src/main/java/com/example/mealtracker/CameraFragment.java
+++ app/src/main/java/com/example/mealtracker/CameraFragment.java
@@ -1,6 +1,8 @@
 package com.example.mealtracker;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -8,10 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.squareup.picasso.Picasso;
+
 
 public class CameraFragment extends Fragment {
 
@@ -21,12 +27,21 @@ public class CameraFragment extends Fragment {
     private static final int INSTANT_FRAGMENT_NUM = 0;
     private static final int IMPORT_FRAGMENT_NUM = 1;
     private static final int CAMERA_REQUEST_CODE = 5;
+    private static final int CAMERA_PERM_CODE = 6;
+
+    private static final int RESULT_OK = -1;
+
+    private ImageView mImageView;
+    private Uri mImageUri;
+    String currentPhotoPath;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_calories_camera, container, false);
         Log.d(TAG, "onCreateView: started.");
+
+        mImageView = view.findViewById(R.id.cameraImageView);
 
         Button launchCameraBtn = (Button) view.findViewById(R.id.launchCameraBtn);
         launchCameraBtn.setOnClickListener(new View.OnClickListener() {
@@ -45,7 +60,6 @@ public class CameraFragment extends Fragment {
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                 }
-                //}
             }
         });
         return view;
@@ -53,12 +67,13 @@ public class CameraFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
 
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            Log.d(TAG, "onActivityResult: done taking a photo.");
-            Log.d(TAG, "onActivityResult: attempting to navigate to final share screen.");
-            // navigate to the final share screen to publish photo
+            //mImageUri = data.getData();
+            //Picasso.with(getActivity()).load(mImageUri).into((ImageView) getActivity().findViewById(R.id.cameraImageView));
+
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            mImageView.setImageBitmap(image);
         }
     }
 }
