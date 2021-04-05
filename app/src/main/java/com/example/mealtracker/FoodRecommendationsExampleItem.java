d39bdb978219dfb6415f8c45dbeac79de88abe7b
potassiumnitrate <berlynn_neo@hotmail.com>
2021-04-06 03:00:17 +0800

    foodRecommendations can have more cardviews now

---
+++ app/src/main/java/com/example/mealtracker/FoodRecommendationsExampleItem.java
@@ -0,0 +1,22 @@
+package com.example.mealtracker;
+
+import android.widget.TextView;
+
+public class FoodRecommendationsExampleItem {
+
+    private TextView mText1;
+    private TextView mText2;
+
+    public FoodRecommendationsExampleItem(TextView text1, TextView text2) {
+        mText1 = text1;
+        mText2 = text2;
+    }
+
+    public TextView getText1() {
+        return mText1;
+    }
+
+    public TextView getText2() {
+        return mText2;
+    }
+}
--- app/src/main/java/com/example/mealtracker/foodRecommendations.java
+++ app/src/main/java/com/example/mealtracker/foodRecommendations.java
@@ -4,14 +4,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.*;
 
 public class foodRecommendations extends Fragment {
+
+    private ArrayList<FoodRecommendationsExampleItem> mExampleList;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private TextView mText1;
+    private TextView mText2;
+    private TextView mText3;
 
     @Nullable
     @Override
@@ -27,7 +40,23 @@ public class foodRecommendations extends Fragment {
         HashMap<String, String> recommendedFoods = recommender.getRecommendedFoods();
          */
 
-        // TODO: add more cardviews with food retrieved from database
+        mExampleList = new ArrayList<>();
+        mExampleList.add(new FoodRecommendationsExampleItem(mText1, mText2));
+
+        mRecyclerView = v.findViewById(R.id.myMealsRecyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(v.getContext());
+        mAdapter = new FoodRecommendationsExampleAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // TODO: get real data from database
         return v;
+    }
+
+    // get mExampleList size
+    public int getExampleListSize() {
+        return mExampleList.size();
     }
 }
