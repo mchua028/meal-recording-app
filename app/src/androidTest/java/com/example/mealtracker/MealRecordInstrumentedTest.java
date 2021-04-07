3e098f22e0835936c89c7b33c2fc1f037763912c
Chris Tang <christang_1023@outlook.com>
2021-04-07 23:14:11 +0800

    Add runnable test code for MealRecord Add

--- app/src/androidTest/java/com/example/mealtracker/MealRecordInstrumentedTest.java
+++ app/src/androidTest/java/com/example/mealtracker/MealRecordInstrumentedTest.java
@@ -3,12 +3,16 @@ package com.example.mealtracker;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.example.mealtracker.DAO.MealRecord;
+import com.example.mealtracker.Exceptions.EmptyInputException;
+import com.example.mealtracker.Exceptions.EmptyResultException;
+import com.example.mealtracker.Exceptions.ValueCannotBeNonPositiveException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 
 /**
@@ -19,40 +23,40 @@ import java.time.LocalDateTime;
 public class MealRecordInstrumentedTest {
     private Database d = Database.getSingleton();
 
-//    @Test
-//    public MealRecord post_MealRecord() {
-//        ArrayList<Food> foods = getFoods();
-//        MealRecord mealRecord = null;
-//        try {
-//            mealRecord = new MealRecord(foods, LocalDateTime.now());
-//        } catch (ValueCannotBeNonPositiveException e) {
-//            e.printStackTrace();
-//        }
-//        d.postNewMealRecord(mealRecord);
-//        return mealRecord;
-//   }
-//
-//    private ArrayList<Food> getFoods() {
-//        String[] foodNames = new String[3];
-//        ArrayList<Food> foods = new ArrayList<>();
-//        foodNames[0] = "Steak";
-//        foodNames[1] = "Subway Sandwich";
-//        foodNames[2] = "Apple";
-//
-//        for (int i = 0; i < foodNames.length; i++) {
-//            try {
-//                Food newFood = Food.searchFood(foodNames[i]);
-//                newFood.setActualIntake(15);
-//                foods.add(newFood);
-//            } catch (EmptyInputException e) {
-//
-//            } catch (EmptyResultException e) {
-//                System.out.printf("%s is not found", foodNames[i]);
-//            }
-//
-//        }
-//        return foods;
-//    }
+    @Test
+    public void post_MealRecord() {
+        ArrayList<Food> foods = getFoods();
+        MealRecord mealRecord = null;
+        try {
+            mealRecord = new MealRecord(foods, LocalDateTime.now());
+        } catch (ValueCannotBeNonPositiveException e) {
+            e.printStackTrace();
+        }
+        d.postNewMealRecord(mealRecord);
+   }
+
+    private ArrayList<Food> getFoods() {
+        String[] foodNames = new String[3];
+        ArrayList<Food> foods = new ArrayList<>();
+        foodNames[0] = "Steak";
+        foodNames[1] = "Subway Sandwich";
+        foodNames[2] = "Apple";
+
+        for (int i = 0; i < foodNames.length; i++) {
+            try {
+                Food newFood = Food.searchFood(foodNames[i]);
+                newFood.setActualIntake(15);
+                newFood.setActualIntake(10);
+                foods.add(newFood);
+            } catch (EmptyResultException e) {
+                System.out.printf("%s is not found", foodNames[i]);
+            } catch (EmptyInputException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return foods;
+    }
 
 //    @Test
 //    public void delete_MealRecord() {
@@ -78,19 +82,19 @@ public class MealRecordInstrumentedTest {
 //        LocalDate endDate = LocalDate.of(2021, 4, 3);
 //        MealRecord mealRecord = MealRecord.queryByDate(startDate, endDate)[0];
 //    }
-
-    /**
-     * TODO:
-     *  - add more test case like edit number of food
-     */
-    @Test
-    public void update_MealRecord() {
-        LocalDate startDate = LocalDate.of(2021, 4, 2);
-        LocalDate endDate = LocalDate.of(2021, 4, 3);
-        MealRecord mealRecord = MealRecord.queryByDate(startDate, endDate)[0];
-
-        LocalDateTime dt = LocalDateTime.of(2020,5,20,13,14,9);
-        mealRecord.setTime(dt);
-        mealRecord.updateToServer();
-    }
+//
+//    /**
+//     * TODO:
+//     *  - add more test case like edit number of food
+//     */
+//    @Test
+//    public void update_MealRecord() {
+//        LocalDate startDate = LocalDate.of(2021, 4, 2);
+//        LocalDate endDate = LocalDate.of(2021, 4, 3);
+//        MealRecord mealRecord = MealRecord.queryByDate(startDate, endDate)[0];
+//
+//        LocalDateTime dt = LocalDateTime.of(2020,5,20,13,14,9);
+//        mealRecord.setTime(dt);
+//        mealRecord.updateToServer();
+//    }
 }
