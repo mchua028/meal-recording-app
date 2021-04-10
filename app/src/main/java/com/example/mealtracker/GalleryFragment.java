713e864d467ad522a9a4775dd6fadd68f858374f
potassiumnitrate <berlynn_neo@hotmail.com>
2021-04-11 06:39:26 +0800

    upload image from gallery

--- app/src/main/java/com/example/mealtracker/GalleryFragment.java
+++ app/src/main/java/com/example/mealtracker/GalleryFragment.java
@@ -1,20 +1,33 @@
 package com.example.mealtracker;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 
@@ -26,9 +39,13 @@ public class GalleryFragment extends Fragment {
     private static final int RESULT_OK = -1;
     private Button mChooseImageBtn;
     private Button mUploadBtn;
+    private EditText mEditTextFileName;
     private ImageView mImageView;
     private ProgressBar mProgressBar;
     private Uri mImageUri;
+
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
 
     @Nullable
     @Override
@@ -39,8 +56,12 @@ public class GalleryFragment extends Fragment {
 
         mChooseImageBtn = view.findViewById(R.id.chooseImageBtn);
         mUploadBtn = view.findViewById(R.id.uploadBtn);
+        mEditTextFileName = view.findViewById(R.id.editTextFileName);
         mImageView = view.findViewById(R.id.imageView);
         mProgressBar = view.findViewById(R.id.progressBar);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference("Uploads");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Uploads");
         
         mChooseImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +73,7 @@ public class GalleryFragment extends Fragment {
         mUploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                uploadFile();
             }
         });
         
@@ -75,6 +96,60 @@ public class GalleryFragment extends Fragment {
             mImageUri = data.getData();
 
         Picasso.with(getActivity()).load(mImageUri).into(mImageView);
+        }
+    }
+
+    /**
+     * Returns extension name of file e.g. jpeg, png
+     * @param uri
+     * @return extension
+     */
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (mImageUri != null) {
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+                + "." + getFileExtension(mImageUri));   // e.g. name of file = timeInMilliseconds.jpeg
+
+            fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress(0);
+                                }
+                            }, 5000);
+
+                            Toast.makeText(getActivity(), "Upload successful", Toast.LENGTH_LONG).show();
+                            GalleryFragmentUpload upload = new GalleryFragmentUpload(mEditTextFileName.getText().toString().trim(),
+                                    taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+                            String uploadId = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                            mProgressBar.setProgress((int) progress);
+                        }
+                    });
+        }
+        else {
+            Toast.makeText(getActivity(), "No file selected", Toast.LENGTH_SHORT).show();
         }
     }
 }
---
+++ app/src/main/java/com/example/mealtracker/GalleryFragmentUpload.java
@@ -0,0 +1,34 @@
+package com.example.mealtracker;
+
+public class GalleryFragmentUpload {
+    private String mName;
+    private String mImageUrl;
+
+    public GalleryFragmentUpload() {
+        // empty constructor needed
+    }
+
+    public GalleryFragmentUpload(String name, String imageUrl) {
+        if (name.trim().equals("")) {
+            name = "No Name";
+        }
+        mName = name;
+        mImageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        mImageUrl = imageUrl;
+    }
+}
